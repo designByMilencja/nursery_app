e81614075f68a65967371a3121cef10025dc6d95
Milena Pieńkosz <designbymilencja@gmail.com>
2023-12-12 12:42:27 +0300

    installed shadcn and auth and theme provider

---
+++ constants/index.ts
@@ -0,0 +1,68 @@
+import { SidebarLink } from "@/types";
+
+export const sidebarLinks: SidebarLink[] = [
+  {
+    imgURL: "/assets/icons/newspaper.svg",
+    route: "/",
+    label: "Aktualności",
+  },
+  {
+    imgURL: "/assets/icons/briefcase.svg",
+    route: "/job",
+    label: "Praca",
+  },
+  {
+    imgURL: "/assets/icons/mail.svg",
+    route: "/contact",
+    label: "Kontakt",
+  },
+];
+export const sidebarLinksEmployer: SidebarLink[] = [
+  {
+    imgURL: "/assets/icons/newspaper.svg",
+    route: "/",
+    label: "Aktualności",
+  },
+  {
+    imgURL: "/assets/icons/briefcase.svg",
+    route: "/job",
+    label: "Praca",
+  },
+  {
+    imgURL: "/assets/icons/person.svg",
+    route: "/account",
+    label: "Konto",
+  },
+  {
+    imgURL: "/assets/icons/mail.svg",
+    route: "/contact",
+    label: "Kontakt",
+  },
+];
+export const sidebarLinksEmployeeAdmin: SidebarLink[] = [
+  {
+    imgURL: "/assets/icons/newspaper.svg",
+    route: "/",
+    label: "Aktualności",
+  },
+  {
+    imgURL: "/assets/icons/briefcase.svg",
+    route: "/job",
+    label: "Praca",
+  },
+  {
+    imgURL: "/assets/icons/school.svg",
+    route: "/procedures",
+    label: "Procedury",
+  },
+  {
+    imgURL: "/assets/icons/person.svg",
+    route: "/account",
+    label: "Konto",
+  },
+  {
+    imgURL: "/assets/icons/mail.svg",
+    route: "/contact",
+    label: "Kontakt",
+  },
+];
